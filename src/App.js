c2d0a04dd2727196c3632632fc6126fdf6801e5a
Jonas Wulf-Andersen <jonas.wulfandersen@gmail.com>
2024-06-11 14:38:52 +0200

    Forside sat korrekt op

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import './App.css';
 import React from 'react';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <h1>Velkommen til Livscyklus</h1>
         <p>
-          Dette er en personlig app til Liv for at følge hendes menstruationscyklus.
+          Dette er en personlig app til Liv for at følge hendes cyklus.
         </p>
       </header>
     </div>
@@ -15,34 +15,3 @@ function App() {
 }
 
 export default App;
-
-
-/*
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-
-export default App;
-
-*/
--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-//import App from './App';
-import Calendar from './Calendar'; // Importér Calendar.js
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Calendar />
+    <App />
   </React.StrictMode>
 );
 
